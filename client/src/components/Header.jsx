0fac15256ae28a002538abbef916ad759b68c09c
jesus <Jesusfernandorios93@gmail.com>
2020-11-12 15:52:52 -0700

    setting up Auth0 for authentication, and created user Component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,12 +3,13 @@ import './styling/header.css'
 import PersonIcon from '@material-ui/icons/Person';
 import IconButton from '@material-ui/core/IconButton';
 import ForumIcon from '@material-ui/icons/Forum';
+import {useAuth0} from '@auth0/auth0-react'
 
 
 export default function Header() {
     return (
         <div className='header'>
-            <IconButton>
+            <IconButton onClick = {() => loginWithRedirect()}>
                <PersonIcon fontSize="large" className="header__icon"/> 
             </IconButton>
             
