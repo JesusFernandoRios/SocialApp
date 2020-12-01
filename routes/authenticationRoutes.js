7c19ac08866def4f8076fd9403866bca6361e240
jesus <Jesusfernandorios93@gmail.com>
2020-12-01 15:11:53 -0700

    implemented validatior for register and login. now ill install Bcrypt.js to hash user passswords

--- routes/authenticationRoutes.js
+++ routes/authenticationRoutes.js
@@ -4,14 +4,18 @@ import {registerValidation, loginValidation} from '../validation.js'
 
 let router = Router()
 
-// VALIDATION
-
-
 router.post('/register', async (req, res) => {
-
-    const {error} = schema.validate(req.body)
+    // This validates the information provided by the user and makes sure it meets criteria
+    const {error} = registerValidation(req.body)
     if(error) return res.status(400).send(error.details[0].message)
 
+    // check this the user is already in DB
+    const emailExists = await Users.findOne({
+        email: req.body.email
+    })
+    if(emailExists) return res.status(400).send("Email Already Exists")
+
+    // creates a new user
     const user = new Users({
         name: req.body.name,
         email: req.body.email,
