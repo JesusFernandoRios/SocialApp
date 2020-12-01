1a137760f285f55ce4ec61988995782b13c980b2
jesus <Jesusfernandorios93@gmail.com>
2020-12-01 14:47:36 -0700

    installed hapi/joi package for validation criteria in user promps

--- routes/authenticationRoutes.js
+++ routes/authenticationRoutes.js
@@ -1,10 +1,21 @@
 import { Router } from 'express'
 import Users from '../models/auth.js'
+import Joi from '@hapi/joi'
 
 let router = Router()
 
+// VALIDATION
+const schema = Joi.object({
+    name: Joi.string().min(6).required(),
+    email: Joi.string().min(6).required().email(),
+    password: Joi.string().min(6).required()
+})
+
 router.post('/register', async (req, res) => {
-    res.send("registered")
+
+    const {error} = schema.validate(req.body)
+    if(error) return res.status(400).send(error.details[0].message)
+
     const user = new Users({
         name: req.body.name,
         email: req.body.email,
